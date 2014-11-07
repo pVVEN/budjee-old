703c8752ebcb4e79f8c1adde52617960889bfa45
Owen <owencmcnamara@gmail.com>
2014-11-07 14:45:13 -0500

    Messing with Angular tests

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,30 @@
-function (){
+/*function (){
 	
-}
+}*/
+
+
+/*
+Angular test stuff
+*/
+angular.module('angularTestApp', [])
+	.controller('AngularTestController', function($scope)
+	{
+		$scope.todos = [{
+			text: "Learn AngularJS"
+		}];
+
+		$scope.addTodo = function(todoText)
+		{
+			var todo = {
+				text: todoText
+			};
+
+			$scope.todos.push(todo);
+		}
+
+		$scope.removeTodo = function(index)
+		{
+			$scope.todos.splice(index, 1);
+		}
+	});
+
